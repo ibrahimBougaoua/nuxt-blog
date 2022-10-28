81da65a6bcfbb154742513bc429838d6213a9c96
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2022-10-28 16:50:04 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+
+import Header from '/components/basic/Header.vue';
+import Footer from '/components/basic/Footer.vue';
+
+export default {
+  name : 'default',
+  components : {
+    Header,
+    Footer
+  }
+}
+
+</script>
