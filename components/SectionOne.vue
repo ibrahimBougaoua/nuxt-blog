7ab716d74c626f5b2c26bf25304301328829689f
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2022-11-10 20:12:04 +0100

    ref : section one

--- components/SectionOne.vue
+++ components/SectionOne.vue
@@ -1,34 +1,56 @@
 <template>
-        <div class="flex flex-nowrap mt-1">
-            <div class="px-4">
-                <a href="#" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                </a>
+      <div class="grid grid-cols-3 gap-3 mt-5">
+            <div class="flex-col space-y-6">
+                <div>
+                    <h1 class="text-blue-700 font-semibold">Dynamic Trading</h1>
+                    <img class="w-24" src="../images/round.png" />
+                </div>
+                <div>
+                    <h1 class="text-3xl text-blue-600 font-semibold py-2">Start Trading In Few Step</h1>
+                </div>
+                <div>
+                    <button class="bg-cyan-500 border-2 border-cyan-500 hover:bg-gray-400  text-white py-2 px-4 rounded-full w-36 ">
+                        Start Trading
+                    </button>
+                </div>
             </div>
-            <div class="px-4">
-                <a href="#" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                </a>
-            </div>
-            <div class="px-4">
-                <a href="#" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                </a>
-            </div>
-            <div class="px-4">
-                <a href="#" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                </a>
-            </div>
-            <div class="px-4">
-                <a href="#" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                </a>
+            <div class="grid grid-cols-2 grid-rows-2 gap-3 col-span-2">
+                <div class="bg-white shadow-2xl flex justify-center py-5">
+                    <span class="rounded-full w-20 my-5"> 
+                        <img src="../images/imgacuille.PNG" alt="img">
+                    </span> 
+                    <div class="space-y-5"> 
+                        <h1 class="text-xl font-bold">Create Account</h1> 
+                        <p>Sign up With your Email and Mobil in Just 5 Minute</p>
+                    </div>
+                </div>
+                <div class="bg-white shadow-2xl flex justify-center p-5">
+                    <span class="rounded-full w-20 my-5"> 
+                        <img src="../images/bank.PNG" alt="img">
+                    </span> 
+                    <div class="space-y-5"> 
+                        <h1 class="text-xl font-bold">Verify You'r Bank</h1> 
+                        <p>Verify You'r Bank Account in Easy</p>
+                    </div>
+                </div>
+                <div class="bg-white shadow-2xl flex justify-center p-5">
+                    <span class="rounded-full w-20 my-5"> 
+                        <img src="../images/wallet.PNG" alt="img">
+                    </span> 
+                    <div class="space-y-5"> 
+                        <h1 class="text-xl font-bold">add funf in wallet</h1> 
+                        <p>Quickly add Money to You'r Investement Wallet</p>
+                    </div>
+                </div>
+                <div class="bg-white shadow-2xl flex justify-center p-5">
+                    <span class="rounded-full w-20 my-5"> 
+                        <img src="../images/trade.PNG" alt="img">
+                    </span> 
+                    <div class="space-y-5"> 
+                        <h1 class="text-xl font-bold">Start Trading Instatly</h1> 
+                        <p>Buy and Sell Variety of Top Coin at The Best Price</p>
+                    </div>
+                </div>
             </div>
         </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,18 @@
 <template>
-  <div class="flex justify-between py-10">
-
-    <div class="flex flex-col">
-      <div class="flex flex-col space-y-4">
-        <div>
-          <h1 class="text-blue-700 font-semibold">Dynamic Trading</h1>
-          <img class="w-24" src="../images/round.png" />
-          <h1 class="text-6xl text-blue-600 font-bold">Start Trading With Free Signals Right Now</h1>
-        </div>
-        <div>
-          <p class="text-slate-500">Join World's Biggest And Trusted Exchange.Trade In Bicoin,Ethereum,</p>
-          <p class="text-slate-500">Ripple And Many More Currencies.</p>
-        </div>
-      </div>
-
-      <div class="flex space-x-3 mt-6">
-        <button class="bg-blue-600 hover:bg-cyan-700  text-white py-2 px-4 rounded-full w-36">
-          Start Trading
-        </button>
-        <button class="bg-white border-2 border-cyan-400 text-cyan-400 py-2 px-4 rounded-full w-36 hover:bg-cyan-400 hover:text-white">
-          Dowload App
-        </button>
-      </div>
-
-      <div class="mt-6">
-        <h1 class="text-xl text-blue-900 font-semibold">Featured Companies</h1>
-        <div class="flex space-x-5 mt-3">
-          <NuxtLink to="https://www.bibox.com/" target="_blank">
-            <img class="w-24" src="../images/bibox.png" />
-          </NuxtLink>
-          <NuxtLink to="https://mymonero.com/" target="_blank">
-            <img class="w-24" src="../images/monero.png" />
-          </NuxtLink>
-          <NuxtLink to="https://bitcoin.org/" target="_blank">
-            <img class="w-24" src="../images/bitcoindumy.png" />
-          </NuxtLink>
-          <NuxtLink to="https://www.okx.com/" target="_blank">
-            <img class="w-24" src="../images/okex.png" />
-          </NuxtLink>
-        </div>
-      </div>
-
-    </div>
-
-    <div>
-      <img class="w-full h-full" src="../images/header-img.png" alt="image">
-    </div>
-    
-  </div>
+  <Header />
+  <SectionOne />
 </template>
-  
+
 <script>
+  import Navbar from '~/components/Navbar.vue';
+  import Header from '~/components/basic/Header.vue';
+  import SectionOne from '~/components/SectionOne.vue';
+  
   export default {
-    name: 'IndexPage'
+    name: 'IndexPage',
+    components : {
+      Navbar,
+      Header,
+    }
   }
 </script>
